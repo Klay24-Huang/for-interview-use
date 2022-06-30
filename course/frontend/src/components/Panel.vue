cb3a504cc04e6fba92527e81e8150e6dc8ce1e0d
Hao-Xiang Huang <sean200365@hotmail.com>
2022-06-30 22:22:42 +0800

    add vue

---
+++ course/frontend/src/components/Panel.vue
@@ -0,0 +1,25 @@
+<script setup>
+const foo = "123";
+</script>
+
+<template>
+  <div class="flex mx-8">
+    <!-- course info list -->
+    <div class="w-1/2">
+      <div class="text-2xl">Course Information</div>
+      <div>{{ foo }}</div>
+      <!-- course info block -->
+      <div></div>
+    </div>
+    <!-- course controller -->
+    <div class="w-1/2">
+      <div class="text-2xl text-orange-400">Course I Plan To Take</div>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+a {
+  color: #42b983;
+}
+</style>
